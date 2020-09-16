cc26dbf8159492ec178e6aa5927baae602ea88c5
meet2mky <meet2mky@gmail.com>
2020-09-16 15:22:03 +0530

    added hilbert order

---
+++ Hilbert_Order/main.cpp
@@ -0,0 +1,125 @@
+/*****************************************************************
+IN GOD WE TRUST !!   
+
+Author:- meet2mky
+Date: -
+Problem:-
+Institue:- NITA
+******************************************************************/
+#include <iostream>
+#include <bitset>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <utility>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <cassert>
+#include <cctype>
+#include <iomanip>
+#include <stack>
+#include <ctime>
+#include <iterator>
+#include <sstream>
+using namespace std;
+#define sync                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define repi(i, a, b) for (int i = (a); i > (b); i--)
+#define all(x) (x).begin(), (x).end()
+#define setall(x, val) fill(all(x), val)
+#define pb push_back
+#define vb vector<bool>
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define sp(x) setprecision(x)
+#define endl '\n'
+#define ll long long
+#define ld long double
+#define SZ(z) (int)(z).size()
+#define inf 0x3f3f3f3f
+#define linf 0x3f3f3f3f3f3f3f3f
+#define eps 0.0000001 // eps = 1e-7
+#define PI 3.141592653589793238
+
+//#define int long long
+const int MOD = 1000000007;
+
+//variadic functions
+template <typename T>
+T sum() { return 0; }
+template <typename T, typename... Args>
+T sum(T a, Args... args) { return a + sum(args...); }
+
+//Debugger  -------------------------------------------->
+#define error(args...)                           \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+
+void err(istream_iterator<string> it)
+{
+}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << " = " << a << endl;
+    err(++it, args...);
+}
+//Debugger  <--------------------------------------------
+/*****************************************************************
+Read the problem carefully!!
+Take inputs carefully
+Care for array index out of bound errors
+Check for overflow
+Divide the problem in several parts if possible
+Keep Calm and believe on yourself.
+Do not panic & work hard you will get it right one day
+******************************************************************/
+const int BLOCK_SIZE = 400;
+struct Query
+{
+    // needs BLOCK_SIZE
+    int l, r, idx;
+    Query(int L = -1, int R = -1, int idx = -1) : l(L), r(R), idx(idx)
+    {
+    }
+    inline pair<int, int> toPair() const
+    {
+        return {l / BLOCK_SIZE, ((l / BLOCK_SIZE) & 1) ? -r : r};
+    }
+};
+inline bool operator<(const Query &a, const Query &b)
+{
+    return a.toPair() < b.toPair();
+}
+
+void solve()
+{
+}
+
+signed main()
+{
+    sync;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
